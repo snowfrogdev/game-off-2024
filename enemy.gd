18cad91f39beca685b26367a109e546c3f6cc929
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2024-11-13 12:08:50 -0500

    Increase trauma value and rename hitbox to hurtbox; add enemy death handling and animations

--- enemy.gd
+++ enemy.gd
@@ -5,9 +5,14 @@ extends CharacterBody2D
 @export var chase_radius = 250.0 # Add chase radius
 @export var attack_radius = 25.0 # Add attack radius
 
+signal enemy_destroyed
+
 var last_direction = Vector2.DOWN # Track the last direction
+var is_dead = false # Track if the enemy is dead
 
 func _process(delta: float) -> void:
+    if is_dead:
+        return # Do nothing if the enemy is dead
     if player and is_player_within_radius():
         if is_player_within_attack_radius():
             perform_attack()
@@ -35,6 +40,8 @@ func perform_attack() -> void:
     play_attack_animation()
 
 func play_animation() -> void:
+    if is_dead:
+        return # Do not play any other animations if the enemy is dead
     if velocity.length() > 0:
         if abs(velocity.x) > abs(velocity.y):
             if velocity.x > 0:
@@ -59,6 +66,8 @@ func play_animation() -> void:
                 $AnimatedSprite2D.play("idle-up")
 
 func play_attack_animation() -> void:
+    if is_dead:
+        return # Do not play attack animation if the enemy is dead
     if abs(last_direction.x) > abs(last_direction.y):
         if last_direction.x > 0:
             $AnimatedSprite2D.play("attack-right")
@@ -69,3 +78,25 @@ func play_attack_animation() -> void:
             $AnimatedSprite2D.play("attack-down")
         else:
             $AnimatedSprite2D.play("attack-up")
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+    if area.is_in_group("Sword"):
+        is_dead = true # Set the enemy as dead
+        play_death_animation()
+
+func play_death_animation() -> void:
+    if abs(last_direction.x) > abs(last_direction.y):
+        if last_direction.x > 0:
+            $AnimatedSprite2D.play("death-right")
+        else:
+            $AnimatedSprite2D.play("death-left")
+    else:
+        if last_direction.y > 0:
+            $AnimatedSprite2D.play("death-down")
+        else:
+            $AnimatedSprite2D.play("death-up")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+    if $AnimatedSprite2D.animation.begins_with("death-"):
+        enemy_destroyed.emit()
+        queue_free()
